7bef4077da6955ab0cc1a6794300c678b9ff2f30
Krzysztof Radomski <krzysztof@radomski.io>
2021-08-26 23:11:10 +0200

    various tweaks

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -3,14 +3,12 @@ const Spinner = () => (
       version="1.1"
       id="L9"
       xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
       viewBox="0 0 100 100"
-      enableBackground="new 0 0 0 0"
-      xmlSpace="preserve"
+      width="calc(100vw - 100px)"
+      height="calc(100vh - 100px)"
     >
       <path
-        fill="black"
+        fill="#7D4CDB"
         d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
       >
         <animateTransform
