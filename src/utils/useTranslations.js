817f9e4486bf939c6c04c7443f2a9094660db844
Krzysztof Radomski <krzysztof@radomski.io>
2021-08-30 20:55:53 +0200

    simplify translations provider

--- src/utils/useTranslations.js
+++ src/utils/useTranslations.js
@@ -1,24 +1,27 @@
-import { useEffect, createContext, useContext } from 'react'
-import { useLocalStorage } from './useLocalStorage'
+import { useEffect, useState, createContext, useContext } from 'react'
 import { CONFIG } from './config'
 import dictionary from './dictionary'
 
 const TranlationsContext = createContext()
 
 function TranslationsProvider({ children }) {
-  const [language, setLanguage] = useLocalStorage('language', '')
-  const languages = CONFIG.languages
+  const [language, setLanguage] = useState(CONFIG.language)
   useEffect(() => {
-    let currentLanguage
-    if (!language && window.navigator?.language) {
-      currentLanguage = window.navigator.language.split('-')[0] || CONFIG.language
-    } else if (language) {
-      currentLanguage = language
+    if (window.navigator?.language) {
+      const language = window.navigator.language.split('-')[0]
+      setLanguage(language)
+      window.document.title = dictionary.math4kids[language]
     }
-    setLanguage(currentLanguage)
-    window.document.title = dictionary.math4kids[currentLanguage]
-  }, [language, setLanguage])
-  const value = { translate: key => dictionary[key]?.[language] || '...', setLanguage, language, languages }
+  }, [])
+  const value = {
+    translate: key => dictionary[key]?.[language] || '...',
+    setLanguage: language => {
+      setLanguage(language)
+      window.document.title = dictionary.math4kids[language]
+    },
+    language,
+    languages: CONFIG.languages
+  }
   return <TranlationsContext.Provider value={value}>{children}</TranlationsContext.Provider>
 }
 function useTranslation() {
