0be261ed8b8ea872d6088ce72811557e442d925b
Krzysztof Radomski <krzysztof@radomski.io>
2021-09-24 13:28:37 +0200

    add 3 more languages support, fix overflow

--- src/utils/dictionary.js
+++ src/utils/dictionary.js
@@ -1,15 +1,34 @@
 const dictionary = {
-  add: { pl: 'Dodawaj', en: 'Add' },
-  subtract: { pl: 'Odejmuj', en: 'Substract' },
-  multiply: { pl: 'Mnóż', en: 'Multipy' },
-  divide: { pl: 'Dziel', en: 'Divide' },
-  score: { pl: 'Punkty', en: 'Score' },
-  math4kids: { pl: 'Matematyka dla dzieci', en: 'Math for kids' },
-  answers: { pl: 'Odpowiedzi', en: 'Answers' },
-  getNewCalc: { pl: 'Nowe zadanie', en: 'Get new calculation' },
-  correct: { pl: 'Poprawna odpowiedź', en: 'Correct answer' },
-  wrong: { pl: 'Niepoprawna odpowiedź', en: 'Wrong answer' },
-  difficulty: { pl: 'Poziom trudności', en: 'Difficulty' }
+  add: { pl: 'Dodawaj', en: 'Add', de: 'Hinzuf.', es: 'Añade', zh: '增加' },
+  subtract: { pl: 'Odejmuj', en: 'Substract', de: 'Substr.', es: 'Substr.', zh: '减去' },
+  multiply: { pl: 'Mnóż', en: 'Multipy', de: 'Multi.', es: 'Multi.', zh: '繁殖' },
+  divide: { pl: 'Dziel', en: 'Divide', de: 'Aust.', es: 'Divide', cn: '分裂' },
+  score: { pl: 'Punkty', en: 'Score', de: 'Punktzahl', es: 'Puntuación', cn: '得分' },
+  math4kids: {
+    pl: 'Matematyka dla dzieci',
+    en: 'Math for kids',
+    de: 'Mathematik für Kinder',
+    es: 'Matemáticas para niños',
+    cn: '孩子们的数学'
+  },
+  answers: { pl: 'Odpowiedzi', en: 'Answers', de: 'Antworten', es: 'Respuestas', cn: '答案' },
+  getNewCalc: { pl: 'Nowe zadanie', en: 'New calculation', de: 'Neue Rechnung', es: 'Nuevo cálculo', cn: '新计算' },
+  correct: {
+    pl: 'Poprawna odpowiedź',
+    en: 'Correct answer',
+    de: 'Richtige Antwort',
+    es: 'Respuesta correcta',
+    zh: '正确答案'
+  },
+  wrong: {
+    pl: 'Niepoprawna odpowiedź',
+    en: 'Wrong answer',
+    de: 'Falsche Antwort',
+    es: 'Respuesta errónea',
+    zh: '错误的答案'
+  },
+  difficulty: { pl: 'Poziom trudności', en: 'Difficulty', de: 'Schwierigkeit', es: 'Dificultad', zh: '难度' },
+  about: {}
 }
 
 export default dictionary
